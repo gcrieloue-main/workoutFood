6d173af97d4fdbff5cb1b4e45499e5f712556a2e
Gilles CRIELOUE <gilles.crieloue@capgemini.com>
2017-03-10 17:20:23 +0100

    food-search

---
+++ src/app/food-search.component.ts
@@ -0,0 +1,46 @@
+import { Typeahead } from 'ng2-typeahead';
+
+@NgModule({
+  declarations: [ Typeahead ],
+})
+
+@Component({
+  selector: 'food-search',
+  template: require('app/food-search.component.html'),
+  styles: [`
+        .typeahead-input,
+        .typeahead-typeahead{
+            width: 250px;
+            padding: 8px;
+            border-radius: 5px;
+        }
+    `]
+})
+export class FoodSearchComponent {
+
+  fruitName: string;
+  fruits: any[] = [
+    {
+      id: 1,
+      name: "Apple",
+      searchText: "apple"
+    },
+    {
+      id: 2,
+      name: "Orange",
+      searchText: "orange"
+    },
+    {
+      id: 3,
+      name: "Banana",
+      searchText: "banana"
+    }
+  ];
+
+  selectedFruit: any = this.fruits[0];
+
+  public fruitSelected(fruit) {
+    this.fruitName = fruit ? fruit.name : 'none';
+  }
+
+}
