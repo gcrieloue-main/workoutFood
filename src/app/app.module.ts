b0014db3d7be9efce095423c322b17f294ffc85a
Gilles CRIELOUE <gilles.crieloue@capgemini.com>
2017-03-10 17:27:00 +0100

    food-search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
 import {FoodComponent} from "./food.component";
 import {MealComponent} from "./meal.component";
+import {FoodSearchComponent} from "./food-search.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule
   ],
-  declarations: [AppComponent, FoodComponent, MealComponent],
+  declarations: [AppComponent, FoodComponent, MealComponent, FoodSearchComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
