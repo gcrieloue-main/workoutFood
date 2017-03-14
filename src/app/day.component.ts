912744d5b863e349c685b5f4c4d789d6f6ac8dcb
hellsdark <hellsdark@gmail.com>
2017-03-14 23:08:23 +0100

    fixes

--- src/app/day.component.ts
+++ src/app/day.component.ts
@@ -3,14 +3,11 @@ import {MealFood} from "./mealFood";
 import {DataService} from "./data.service";
 import {Subscription} from "rxjs/Subscription";
 
-
-
-
-
 @Component({
   selector: 'day',
   templateUrl: 'app/day.component.html'
 })
 export class DayComponent implements OnDestroy {
-
+  meal:Meal = new Meal([]);
+  day:Day = new Day([meal]);
 }
