bee5d563becf603c07bab806090e74fc85155fbb
hellsdark <hellsdark@gmail.com>
2017-03-14 21:48:08 +0100

    fixes

--- src/app/meal.component.ts
+++ src/app/meal.component.ts
@@ -30,6 +30,7 @@ export class MealComponent implements OnDestroy {
 
   computeCalories() {
     console.debug("compute meal calories");
+    console.debug("calories base : ", this.caloriesBase);
     if (this.foods.length > 0) {
       this.caloriesTotal = Math.ceil(this.foods.map((mealFood)=>mealFood.weight * mealFood.food.calories / 100).reduce((c1, c2)=>c1 + c2));
       this.caloriesPercentage = Math.ceil((this.caloriesTotal * 100) / this.caloriesBase);
@@ -38,7 +39,7 @@ export class MealComponent implements OnDestroy {
       this.caloriesTotal = 0;
       this.caloriesPercentage = 0;
     }
-    console.debug("=> total : " + this.caloriesTotal + ", percentage : " + this.caloriesPercentage + "%");
+    console.debug("=> total : %s, percentage : %s" + this.caloriesTotal, this.caloriesPercentage + "%");
   }
 
   remove(mealFood: MealFood) {
