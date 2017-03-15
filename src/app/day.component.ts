f4d8af7d26476aa44211b29725491a7600a0d5e5
Gilles CRIELOUE <gilles.crieloue@capgemini.com>
2017-03-15 14:25:55 +0100

    fixes

--- src/app/day.component.ts
+++ src/app/day.component.ts
@@ -1,20 +1,20 @@
 import {Component, Input, OnDestroy, ViewChildren} from "@angular/core";
 import {Day} from "./day";
-import {DataService} from './data.service';
+import {DataService} from "./data.service";
 
 @Component({
   selector: 'day',
   templateUrl: 'app/day.component.html'
 })
 export class DayComponent implements OnDestroy {
-  day:Day;
+  day: Day;
 
-  constructor(private dataService:DataService) {
-    this.day = dataService.newDay();
+  constructor(private dataService: DataService) {
+    this.day = this.dataService.newDay();
     this.dataService.newMeal(this.day);
   }
 
-  addMeal():void {
+  addMeal(): void {
     this.dataService.newMeal(this.day);
   }
 }
