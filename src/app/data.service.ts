87c8e6b9cf9473225278eb8b2414e6e5d12c558e
hellsdark <hellsdark@gmail.com>
2017-03-15 13:41:49 +0100

    fixes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -58,4 +58,11 @@ export class DataService {
     }
     return null;
   }
+
+  newGuid():string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 }
