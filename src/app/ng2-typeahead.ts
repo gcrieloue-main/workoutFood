de3517b8a7539aef372ff8794e910067ecdcbf51
Gilles CRIELOUE <gilles.crieloue@capgemini.com>
2017-03-17 10:22:36 +0100

    change folder structure

--- src/app/ng2-typeahead.ts
+++ src/app/ng2-typeahead.ts
@@ -19,7 +19,7 @@ export const TYPEAHEAD_CONTROL_VALUE_ACCESSOR:any = {
 @Component({
   selector: 'typeahead',
   templateUrl: 'app/ng2-typeahead.html',
-  styleUrls: ['assets/css/ng2-typeahead.css'],
+  styleUrls: ['ng2-typeahead.css'],
   providers: [TYPEAHEAD_CONTROL_VALUE_ACCESSOR]
 })
 export class Typeahead implements OnInit, ControlValueAccessor {
