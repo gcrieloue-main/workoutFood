604ccf52097a5127000c90169b9b947a4d84eb24
hellsdark <hellsdark@gmail.com>
2017-03-18 17:37:18 +0100

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: `<router-outlet></router-outlet>`
+  template: `<header>
+  <div class="container">
+    <h1>Workout Food</h1>
+  </div>
+</header>
+<router-outlet></router-outlet>`
 })
 export class AppComponent {
 }
