e4a3acb21957da1131ba45884d30a9a8c136c22a
hellsdark <hellsdark@gmail.com>
2017-03-19 22:26:56 +0100

    add custom food feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<header>
   <div class="container">
+  <div class="d-flex flex-row justify-content-between"
     <h1>Workout Food</h1>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="core">Menu<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="foods">Aliments<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
   </div>
 </header>
 <router-outlet></router-outlet>`
