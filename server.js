69470eb108f7a36506ad4c73f7b7b469c7216ede
hellsdark <hellsdark@gmail.com>
2017-03-22 23:43:15 +0100

    dev server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 var rootPath = __dirname;
 
-app.use(express.static(rootPath + '/client/app'))
+app.use(express.static(rootPath + '/src'))
 app.use('/node_modules', express.static(rootPath + '/node_modules'));
 
 var port = 3000;
