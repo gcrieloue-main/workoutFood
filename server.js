9278b6a967138282448b6dfb36940630d9a00d55
hellsdark <hellsdark@gmail.com>
2017-03-22 23:35:06 +0100

    init server

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var mysql = require('mysql');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require('path');
 
