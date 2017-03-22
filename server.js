37f82a05f8266fe1cb7cd160a107cb31df090395
hellsdark <hellsdark@gmail.com>
2017-03-22 23:52:46 +0100

    dev server

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 var express = require('express');
-
+var path = require('path');
 
 var app = express();
 
-var rootPath = __dirname;
+var rootPath = path.normalize(__dirname);
+var appPath = rootPath + "/src";
 
-app.use(express.static(rootPath + '/src'))
+app.use(express.static(appPath);
 app.use('/node_modules', express.static(rootPath + '/node_modules'));
 
 app.get('/', function (req, res) {
   console.log("access to /");
-  res.sendFile(rootPath + '/src/index.html');
+  res.sendFile(appPath + '/index.html');
 });
 
 var port = 3000;
 var server = app.listen(port, function () {
   console.log("run server on port :" + port);
   console.log("routPath: " + rootPath);
+  console.log("appPath: " + appPath);
 });
 
 module.exports = app;
