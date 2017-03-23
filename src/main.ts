29851136ac53c15b80ec0c630c34a9c9de2c62c1
Gilles CRIELOUE <gilles.crieloue@capgemini.com>
2017-03-23 17:08:14 +0100

    enable prod mod

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {enableProdMode} from "@angular/core";
+import {AppModule} from "./app/app.module";
 
 if (/www\.workout-food\.com/.test(document.location.host)) {
   enableProdMode();
 }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
