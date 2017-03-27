f8adb8cb3186f2d1b7912c865936ed24096bc296
hellsdark <hellsdark@gmail.com>
2017-03-27 21:54:31 +0200

    prepare for AOT

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {enableProdMode} from "@angular/core";
-import {AppModuleNgFactory} from "../aot/src/app/app.moedule.ngFactory";
+import {AppModuleNgFactory} from "../aot/src/app/app.module.ngFactory";
 
 if (/www\.workout-food\.com/.test(document.location.host)) {
   enableProdMode();
