50737f4f34b0387d677dbde10e6611263c2c8fd8
hellsdark <hellsdark@gmail.com>
2017-03-29 00:17:02 +0200

    develop summary component

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnDestroy, ViewChildren} from "@angular/core";
 import {DataService} from "../shared/data.service";
+import {Meal} from '../meal/meal';
 
 @Component({
   moduleId: module.id,
@@ -8,7 +9,15 @@ import {DataService} from "../shared/data.service";
 })
 export class SummaryComponent {
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService:DataService) {
+    this.subscription = this.dataService.mealChanged$.subscribe(
+      (meal:Meal) => {
+
+      });
+    this.subscriptionCalories = this.dataService.caloriesBaseChanged$.subscribe(
+      (calories:number) => {
+
+      });
   }
 
 }
