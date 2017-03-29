8708f8bd51196a7c960f7b39770f2dc1d28f3557
hellsdark <hellsdark@gmail.com>
2017-03-29 23:02:27 +0200

    develop summary component

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -11,29 +11,29 @@ import {MealFood} from "../shared/mealFood";
   templateUrl: './summary.component.html'
 })
 export class SummaryComponent {
-  subscription: Subscription;
-  subscriptionCalories: Subscription;
+  subscription:Subscription;
+  subscriptionCalories:Subscription;
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService:DataService) {
     this.subscription = this.dataService.mealChanged$.subscribe(
-      (meal: Meal) => {
+      (meal:Meal) => {
         this.compute();
       });
     this.subscriptionCalories = this.dataService.caloriesBaseChanged$.subscribe(
-      (calories: number) => {
+      (calories:number) => {
         this.compute();
       });
   }
 
-  compute(): void {
-    var proteins: number = 0;
-    var glucids: number = 0;
-    var lipids: number = 0;
-    var fats: number = 0;
+  compute():void {
+    var proteins:number = 0;
+    var glucids:number = 0;
+    var lipids:number = 0;
+    var fats:number = 0;
     var days = this.dataService.getDays();
-    for (let day: Day in days) {
-      for (let meal: Meal in day) {
-        for (let food: MealFood in meal.mealFoods) {
+    for (let day:Day in days) {
+      for (let meal:Meal in day) {
+        for (let food:MealFood in meal.mealFoods) {
           proteins += food.weight * food.food.proteins;
           glucids += food.weight * food.food.proteins;
           lipids += food.weight * food.food.proteins;
@@ -42,20 +42,25 @@ export class SummaryComponent {
       }
     }
 
+    console.log("proteins: " + proteins +
+      "glucids:" + glucids +
+      " , lipids:" + lipids +
+      ", fats:" + fats
+    );
     this.doughnutChartData = [proteins, glucids, lipids, fats];
   }
 
   // Doughnut
-  public doughnutChartLabels: string[] = ['Proteins', 'Glucids', 'Lipids', 'Fats'];
-  public doughnutChartData: number[] = [350, 450, 100, 100];
-  public doughnutChartType: string = 'doughnut';
+  public doughnutChartLabels:string[] = ['Proteins', 'Glucids', 'Lipids', 'Fats'];
+  public doughnutChartData:number[] = [350, 450, 100, 100];
+  public doughnutChartType:string = 'doughnut';
 
   // events
-  public chartClicked(e: any): void {
+  public chartClicked(e:any):void {
     console.log(e);
   }
 
-  public chartHovered(e: any): void {
+  public chartHovered(e:any):void {
     console.log(e);
   }
 
