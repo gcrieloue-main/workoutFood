824b81e4ba0ad6e8daccedca0ac5f080d70a0ccd
hellsdark <hellsdark@gmail.com>
2017-04-02 23:01:21 +0200

    add missing charts js to aot files

--- copy-dist-files.js
+++ copy-dist-files.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var resources = [
   'node_modules/core-js/client/shim.min.js',
   'node_modules/zone.js/dist/zone.min.js',
+  'node_modules/chart.js/dist/Chart.bundle.min.js',
   'src/assets/styles.css'
 ];
 resources.map(function (f) {
