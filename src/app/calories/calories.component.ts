9c9832925d3f0aa6de8815b5672209d87ca45ed3
Gilles CRIELOUE <gilles.crieloue@capgemini.com>
2017-04-04 18:05:39 +0200

    improve calories component

--- src/app/calories/calories.component.ts
+++ src/app/calories/calories.component.ts
@@ -167,7 +167,7 @@ export class CaloriesComponent {
       this.calories = Math.ceil(metabolicRate * this.profile.activityIntensity);
       this.profileForm.patchValue({calories: this.calories});
       this.dataService.setCaloriesBase(this.calories);
-      this.onCaloriesChange(this.calories);
+      this.setCaloriesBase(this.calories);
     }
   }
 
