9e0e8530d0aca635914cd2308b1a6d71e7912b77
Gilles CRIELOUE <gilles.crieloue@capgemini.com>
2017-04-04 17:30:07 +0200

    temp server fix

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var appPath = rootPath + "/aot";
 
 app.use(express.static(appPath));
 app.use('/fonts', express.static(rootPath + '/src/assets/fonts'));
+app.use('/assets/fonts', express.static(rootPath + '/src/assets/fonts'));
 app.use('/img', express.static(rootPath + '/src/assets/img'));
+app.use('/assets/img', express.static(rootPath + '/src/assets/img'));
 app.get('/*', function (req, res) {
   res.sendFile(appPath + '/index.html');
 });
