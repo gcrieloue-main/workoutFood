21bc15abd1eef72870ebbef74b666dfa07ba7e6c
Gilles CRIELOUE <gilles.crieloue@capgemini.com>
2017-04-06 11:36:04 +0200

    add sitemap

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use('/fonts', express.static(rootPath + '/src/assets/fonts'));
 app.use('/assets/fonts', express.static(rootPath + '/src/assets/fonts'));
 app.use('/img', express.static(rootPath + '/src/assets/img'));
 app.use('/assets/img', express.static(rootPath + '/src/assets/img'));
+app.use('/sitemap.xml', express.static(rootPath + '/sitemap.xml'));
 app.get('/*', function (req, res) {
   res.sendFile(appPath + '/index.html');
 });
