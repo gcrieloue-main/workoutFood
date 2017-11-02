2aaca20f7ab2b6f2ad365eef6c4d409a15335fb6
Ubuntu <ubuntu@ip-172-31-30-106.us-east-2.compute.internal>
2017-11-02 14:15:48 +0000

    remove unstable google oauth2 feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,17 @@
 import {Component, NgZone} from '@angular/core';
 
-declare var gapi: any;
-
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  googleLoginButtonId = "google-login-button";
 
   constructor(private _zone: NgZone) {
 
   }
 
   ngAfterViewInit() {
-    gapi.signin2.render(
-      this.googleLoginButtonId,
-      {
-        "onSuccess": this.onGoogleLoginSuccess,
-        "scope": "profile",
-        "theme": "dark"
-      }
-    );
   }
 
-  onGoogleLoginSuccess = (loggedInUser) => {
-    this._zone.run(() => {
-      this.userAuthToken = loggedInUser.getAuthResponse().id_token;
-      this.userDisplayName = loggedInUser.getBasicProfile().getName();
-      console.log(this);
-    });
-  }
 }
